eaccfd9f4e70d1691f76f45ab2fe5b88803d0130
tianmin757 <tianmin757020@sina.com>
2017-03-07 22:00:36 +0800

    Create items.py

---
+++ eastmoney/eastmoney/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy.item import Item, Field
+
+
+class EastmoneyItem(Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    market_cap=Field() #总市值
+    net_margin=Field() #净利润
+    net_assets=Field() #净资产
+    gross_profit_rate=Field() #毛利率
+    stock_id=Field()   #股票代码
+    name=Field()       #股票名称
