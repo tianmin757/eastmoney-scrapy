993cdb9111d2d0d5e3f69e14486274900ef1a2bc
tianmin757 <tianmin757020@sina.com>
2017-03-08 17:21:05 +0800

    Update settings.py

--- eastmoney/eastmoney/settings.py
+++ eastmoney/eastmoney/settings.py
@@ -46,7 +46,7 @@ DEFAULT_REQUEST_HEADERS = {
              "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
              "Accept-Language":"zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
              "Accept-Encoding":"gzip, deflate",
-             "Referer":"http://quote.eastmoney.com/sz002851.html",
+             "Referer":"http://www.eastmoney.com/",
              "Connection":"keep-alive"
 }
 
@@ -99,8 +99,44 @@ ITEM_PIPELINES = {
 
 #USER_AGENT_LIST=['Mozilla/5.0 (Windows NT 6.1; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0',]
 
+#启用Redis调度请求队列
+SCHEDULER='scrapy_redis.scheduler.Scheduler'
+#确保所有的爬虫通过Redis去重
+DUPLEFILTER='scrapy_redis.dupefilter.REFDUPEFilter'
+#默认请求序列化使用的是pickle,但是我们可以更改为其他类似的。PS:这玩意儿2.x可以用，3.x的不能用
+#SCHEDULER_SERIALIZER="scrapy_redis.picklecompat"
+#不清除Redis队列，这样可以暂停/恢复 爬取
+SCHEDULER_PERSIST=True
+#使用优先级调度请求队列(默认使用)
+SCHEDULER_QUEUE_CLASS='scrapy_redis.queue.PriorityQueue'
+#可选用的其他队列
+#SCHEDULER_QUEUE_CLASS='scrapy_redis.queue.FifoQueue'
+#SCHEDULER_QUEUE_CLASS='scrapy_redis.queue.LifoQueue'
+#最大空闲时间防止分布式爬虫因为等待而关闭
+SCHEDULER_IDLE_BEFORE_CLOSE=10
+#序列化项目管道作为Redis Key存储
+#REDIS_ITEMS_KEY='%(spider)s:items'
+#默认使用ScrapyJSONEncoder进行项目序列化
+#You can use any importable path to a callable object.
+#REDIS_ITEMS_SERIALIZER='json.dumps'
+#指定连接到Redis时使用的端口和地址（可选）
 REDIS_HOST='127.0.0.1'
 REDIS_PORT='6379'
+#指定用于连接Redis的URL（可选）
+#如果设置此项优先级高于REDIS_HOST和REDIS_PORT
+#REDIS_URL='redis://user:pass@hostname:9001'
+#自定义的Redis参数（连接超时之类的）
+#REDIS_PARAMS={}
+#自定义redis客户端类
+#REDIS_PARAMS['redis_cls']='myproject.RedisClient'
+#如果为True,则使用redis的'spop'进行操作
+#如果需要避免起始网址列表出现重复，这个选项非常有用。开启此选项urls必须通过sadd添加，否则会出现类型错误。
+#REDIS_START_URLS_AS_SET=False
+#RedisSpider和RedisCrawlSpider默认start_urls键
+#REDIS_START_URLS_KEY='%(name)s:start_urls'
+#设置redis使用utf-8之外的编码
+#REDIS_ENCODING='latin1'
+
 
 MONGODB_HOST='127.0.0.1'
 MONGODB_PORT=27017
