03e48b3fa2d4e7d91520fb4fcd0fd8e8394c9742
tianmin757 <tianmin757020@sina.com>
2017-03-13 10:44:50 +0800

    Update items.py

--- eastmoney/eastmoney/items.py
+++ eastmoney/eastmoney/items.py
@@ -11,9 +11,10 @@ from scrapy.item import Item, Field
 class EastmoneyItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    market_cap=Field() #总市值
-    net_margin=Field() #净利润
-    net_assets=Field() #净资产
-    gross_profit_rate=Field() #毛利率
-    stock_id=Field()   #股票代码
-    name=Field()       #股票名称
+    market_cap=Field()          #总市值
+    net_margin=Field()          #净利润
+    net_assets=Field()          #净资产
+    gross_profit_rate=Field()   #毛利率
+    ROE=Field()                 #净资产收益率
+    stock_id=Field()            #股票代码
+    name=Field()                #股票名称
