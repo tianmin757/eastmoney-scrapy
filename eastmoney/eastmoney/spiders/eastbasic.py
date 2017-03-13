2c0197480f76def0b41ef86d6b8f715b7dd2dbfa
tianmin757 <tianmin757020@sina.com>
2017-03-13 10:46:26 +0800

    Update eastbasic.py

--- eastmoney/eastmoney/spiders/eastbasic.py
+++ eastmoney/eastmoney/spiders/eastbasic.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import pymysql
 from eastmoney.items import EastmoneyItem
+from eastmoney.sql import my_sql
 
 class EastbasicSpider(scrapy.Spider):
     name = "eastbasic"
@@ -10,15 +10,7 @@ class EastbasicSpider(scrapy.Spider):
     #            'http://quote.eastmoney.com/SZ000001.html']
     
     def start_requests(self):
-       conn=pymysql.connect(host='localhost',
-                     user='root',
-                     passwd='root',
-                     db='test',
-                     charset='utf8')
-       sql='select stock_url from test.eastmoney_basic_info'
-       cursor=conn.cursor()
-       cursor.execute(sql)
-       b=cursor.fetchall()
+       b=my_sql.select_urls()
        for url in b:
             string_url=''.join(tuple(url))
             yield scrapy.Request(url=string_url,callback=self.parse)  
@@ -30,6 +22,7 @@ class EastbasicSpider(scrapy.Spider):
         item["stock_id"]=response.css("#code::text").extract()
         item["name"]=response.css(".cwzb > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(1) > b:nth-child(1)::text").extract()
         item["gross_profit_rate"]=response.css(".cwzb > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(7)::text").extract()
+        item["ROE"]=response.css(".cwzb > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(9)::text").extract()
         item["net_margin"]=response.css(".cwzb > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(4)::text").extract()
         item["net_assets"]=response.css(".cwzb > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(3)::text").extract()
         item["market_cap"]=response.css(".cwzb > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(2)::text").extract()
