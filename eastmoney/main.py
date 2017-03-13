fb2b4a8a80b0ceefc1526d4a6c8dc148ff6a8c2a
tianmin757 <tianmin757020@sina.com>
2017-03-13 14:37:48 +0800

    Update main.py

--- eastmoney/main.py
+++ eastmoney/main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from scrapy import cmdline
+from scrapy.cmdline import execute
 
-cmdline.execute('scrapy crawl eastbasic'.split())
+execute(['scrapy','crawl','eastbasic'])
